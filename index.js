63a9511858c75527a4e2cfe17cfea81d496b0a3a
bisma202 <164315441+bisma202@users.noreply.github.com>
2024-03-26 13:59:33 -0700

    changes
    changes

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-#! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
     { message: "enter first number", type: "number", name: "firstNumber" },
     { message: "enter second number", type: "number", name: "secondNumber" },
     {
-        message: "select one of the operators to perform opperation",
+        message: "select one of the operator to perform opperation",
         type: "list",
         name: "operator",
         choices: ["addition", "subtraction", "multiplication", "division"],
     },
 ]);
-if (answers.userGuessedNumber === randomNumber) {
-    console.log("congtratulation! you guessed right number.");
+// conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
 }
 else {
-    console.log("you guessed wrong number");
+    console.log("please select valid operator");
 }
